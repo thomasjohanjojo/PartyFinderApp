75a34ee4711f467c0be25310db9b90a74fcef57f
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2025-04-24 19:05:32 +0200

    Added the code for a picture and a button

--- party_finder_app_flutter_project/lib/main.dart
+++ party_finder_app_flutter_project/lib/main.dart
@@ -11,9 +11,27 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Column(children: <Widget>[
-          Center(child: Image.asset("assets/images/masterOfDirtPicture.jpg")),
-        ]),
+        body: Column(
+          children: <Widget>[
+            Center(
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height *
+                    0.7, // Adjust this value as needed (e.g., 0.8, 0.9)
+                child: FittedBox(
+                  fit: BoxFit.fitHeight,
+                  child: Image.asset("assets/images/masterOfDirtPicture.jpg"),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Elevated Button Pressed!');
+                // Add your button's action here
+              },
+              child: const Text('Elevated Button'),
+            ),
+          ],
+        ),
       ),
     );
   }
