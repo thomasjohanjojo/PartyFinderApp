c214c20947829793cb6601ff120e60cfa23e34b6
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2025-04-24 18:45:24 +0200

    Created the flutter project

---
+++ party_finder_app_flutter_project/README.md
@@ -0,0 +1,3 @@
+# party_finder_app_flutter_project
+
+A new Flutter project.
---
+++ party_finder_app_flutter_project/android/app/src/main/kotlin/com/example/party_finder_app_flutter_project/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.party_finder_app_flutter_project
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
