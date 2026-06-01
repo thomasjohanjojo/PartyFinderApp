b7bdf4aa8f9d1bea9e79e666336c8a0058c1275e
TopTargaryen <42695284+thomasjohanjojo@users.noreply.github.com>
2026-06-01 21:53:02 +0200

    Changed the names of the files to align with the names in the code
    Also added the exact address of the flutter app local port address

--- party_finder_app_flutter_project/backend_python_code/RepositoryPatternInterfaces/PosterDetailsDataAPI.py
+++ party_finder_app_flutter_project/backend_python_code/RepositoryPatternInterfaces/PosterDetailsDataAPI.py
@@ -14,10 +14,10 @@ app = FastAPI()
 # ADD THIS BLOCK TO ENABLE CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # In production, replace "*" with your Flutter app's domain
+    allow_origins=["http://localhost:3000"], # In production, replace "*" with your Flutter app's domain
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=["http://localhost:3000"],
+    allow_headers=["http://localhost:3000"],
 )
 
 # --- 1. THE DEPENDENCY PROVIDER ---
